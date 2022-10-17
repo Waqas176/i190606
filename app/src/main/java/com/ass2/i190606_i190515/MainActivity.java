89c12565418f86b6d61147f9b34233314a81ecb4
Waqas176 <i190606@nu.edu.pk>
2022-10-18 02:48:40 +0500

    signup page

--- app/src/main/java/com/ass2/i190606_i190515/MainActivity.java
+++ app/src/main/java/com/ass2/i190606_i190515/MainActivity.java
@@ -10,15 +10,15 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
 
-    Button abcd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//            new Handler().postDelayed(() -> {
-//                startActivity(new Intent(this, MainActivity2.class));
-//                finish();
-//            }, 2000);
+            new Handler().postDelayed(() -> {
+                startActivity(new Intent(this, MainActivity2.class));
+                finish();
+            }, 2000);
 
     }
 }
--- app/src/main/java/com/ass2/i190606_i190515/MainActivity2.java
+++ app/src/main/java/com/ass2/i190606_i190515/MainActivity2.java
@@ -1,12 +1,29 @@
 package com.ass2.i190606_i190515;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    EditText name,email,password;
+    ImageButton signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +33,72 @@ public class MainActivity2 extends AppCompatActivity {
 //            startActivity(new Intent(this, MainActivity3.class));
 //            finish();
 //        }, 2000);
+
+        mAuth=FirebaseAuth.getInstance();
+        name=findViewById(R.id.name);
+        email=findViewById(R.id.email);
+        password=findViewById(R.id.password);
+        signup=findViewById(R.id.signup);
+
+
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mAuth.createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful())
+                                {
+                                    Toast.makeText(
+                                            MainActivity2.this,
+                                            "Successfully Created User",
+                                            Toast.LENGTH_LONG
+                                    ).show();
+                                    Toast.makeText(
+                                            MainActivity2.this,
+                                            mAuth.getCurrentUser().getUid(),
+                                            Toast.LENGTH_LONG
+                                    ).show();
+
+
+                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(
+                                        MainActivity2.this,
+                                        "Failed to Create User",
+                                        Toast.LENGTH_LONG
+
+                                ).show();
+                            }
+                        });
+
+            }
+        });
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user!=null)
+        {
+            Toast.makeText(
+                    MainActivity2.this,
+                    user.getUid()+"",
+                    Toast.LENGTH_SHORT
+                    ).show();
+        }
+
+        
+        
+        
     }
 }
