ee74771d62e22017eacf86d721cfa2b8e901b46f
Waqas176 <i190606@nu.edu.pk>
2022-10-17 20:16:00 +0500

    commit button

--- app/src/main/java/com/ass2/i190606_i190515/MainActivity.java
+++ app/src/main/java/com/ass2/i190606_i190515/MainActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button abc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
