f9084d46aedd16aadc3958f52695ee618b89ae96
Waqas176 <i190606@nu.edu.pk>
2022-10-18 03:08:12 +0500

    music play + signup auth

--- app/src/main/java/com/ass2/i190606_i190515/MainActivity.java
+++ app/src/main/java/com/ass2/i190606_i190515/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
             new Handler().postDelayed(() -> {
-                startActivity(new Intent(this, MusicList.class));
+                startActivity(new Intent(this, MainActivity2.class));
                 finish();
             }, 2000);
 
