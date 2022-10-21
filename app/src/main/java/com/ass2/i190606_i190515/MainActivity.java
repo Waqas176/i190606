2543ac29f375f03ad7471e9518e8dc778589413e
Waqas176 <i190606@nu.edu.pk>
2022-10-21 15:54:25 +0500

    sign up object form3

--- app/src/main/java/com/ass2/i190606_i190515/MainActivity.java
+++ app/src/main/java/com/ass2/i190606_i190515/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
 
-    Button abcdef;
+    Button abcdefg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
